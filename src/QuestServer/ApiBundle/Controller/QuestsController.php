6d6f5a8823c49a9fceb43b3e9b7ea4c5f03846e3
Jeffrey den Drijver <jeedendree@gmail.com>
2014-06-03 20:27:11 +0200

    Update QuestController in general and add Quest GET call

--- src/QuestServer/ApiBundle/Controller/QuestsController.php
+++ src/QuestServer/ApiBundle/Controller/QuestsController.php
@@ -4,6 +4,8 @@ namespace QuestServer\ApiBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
+use FOS\RestBundle\Controller\Annotations\View;
+
 class QuestsController extends FOSRestController
 {
 
@@ -19,9 +21,46 @@ class QuestsController extends FOSRestController
         
         $quests = $em->getRepository('QuestServerQuestBundle:Quest')->findAll();
 
-        $view = $this->view($quests, 200);
+        $data = array();
+        foreach ($quests as $quest) {
+            $data[] = $this->formatQuest($quest);
+        }
+
+        $view = $this->view($data, 200);
 
         return $this->handleView($view);
+    }
+
+    /**
+     * @ApiDoc(
+     *  description="Retrieve one quest",
+     *  section="Quests"
+     * )
+     */
+    public function getQuestAction($questID)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $quest = $em->getRepository('QuestServerQuestBundle:Quest')->find($questID);
+
+        $data              = $this->formatQuest($quest);
+        $data['questions'] = count($quest->getQuestions());
+
+        $view = $this->view($data, 200);
+
+        return $this->handleView($view);
+    }
+
+    private function formatQuest($entity){
+        $data = array();
+
+        $data['id']             = $entity->getID();
+        $data['name']           = $entity->getName();
+        $data['description']    = $entity->getDescription();
+        $data['valid_from']     = $entity->getValidFrom();
+        $data['valid_untill']   = $entity->getValidUntill();
+
+        return $data;
     }
 
 }
