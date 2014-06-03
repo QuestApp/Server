805b43718343fd9987de902180b41307ba24fa4e
Jeffrey den Drijver <jeedendree@gmail.com>
2014-06-03 21:27:15 +0200

    Add Quest admin interface, fixes #12

---
+++ src/QuestServer/QuestBundle/Form/QuestType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace QuestServer\QuestBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class QuestType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('validFrom')
+            ->add('validUntill')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'QuestServer\QuestBundle\Entity\Quest'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'questserver_questbundle_quest';
+    }
+}
